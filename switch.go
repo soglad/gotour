ad2ea3d59b7d3f8cbd48a24096c04704703e7be7
Tony Wen <lintaowen@gmail.com>
2017-09-30 15:59:40 +0800

    go doc test

--- switch.go
+++ switch.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
+	"fmt"
+	"runtime"
 )
 
 func testSwitch() {
-   switch osname := runtime.GOOS; osname {
-        case "darwin":
-            fmt.Println("Mac OS")
-        case "linux":
-            fmt.Println("Linux")
-        default :
-            fmt.Println("%s", osname)
-   }
-} 
+	switch osname := runtime.GOOS; osname {
+	case "darwin":
+		fmt.Println("Mac OS")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Println("%s", osname)
+	}
+}
 
-func main(){
-   testSwitch()
+func main() {
+	testSwitch()
 }
